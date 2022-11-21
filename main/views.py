e215cf1089e9f2bebf1e13481b81392c4d6c8a45
yandivd <yandivd@gmail.com>
2022-11-21 18:39:14 -0500

    ver HC

--- main/views.py
+++ main/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from .models import *
 from .forms import *
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic import UpdateView
 
 # Create your views here.
 @login_required
 def home(request):
     return render(request, 'main/index.html')
 
+# Historias Clinicas CRUD
 @login_required
 def hclistar(request):
     estadoT = Estado.objects.get(nombre='Activo')
@@ -26,6 +29,25 @@ def crearHC(request):
 
     return render(request, 'gestion/hc/create.html', data)
 
+def verHC(request,pk):
+    paciente = get_object_or_404(Paciente, id=pk)
+    hc = get_object_or_404(Historia_clinica, id=pk)
+    data = {
+        "formP" : PacienteForm(instance=paciente),
+        "formHC" : HCForm(instance=hc),
+    }
+    # if request.method=='POST':
+    #     formulario = PacienteForm(data=request.POST, instance=paciente, files=request.FILES)
+    #     if formulario.is_valid():
+    #         formulario.save()
+    #         messages.success(request,"Modificada Correctamente")
+    #         return redirect(to='home') #te redirige al listado de productos ya editados
+    #     else:
+    #         data["formP"]=formulario
+
+    return render(request,'gestion/hc/view.html', data)
+
+#Cosas de los Pacientes
 @permission_required('main.add_paciente')
 def crearPaciente(request):
     
